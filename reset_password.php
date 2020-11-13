15232d7619ba9aa26ff0bb0c54d65c3661b3dfa6
Bigyan Bista <bigyanyo@gmail.com>
2020-11-13 20:40:15 +0545

    Made changes on reset password

--- reset_password.php
+++ reset_password.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reset Password</title>
-</head>
-<body>
+<?php
+    require "header.php";
+?>
     
     <div>
         <form action="changepassw.php" method="post">
@@ -30,5 +25,6 @@
         </form>
     </div>
     
-</body>
-</html>
+<?php
+    require "footer.php";
+?>
