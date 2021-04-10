a21e85b32f01518c9233c2ebac364a0686429038
Bigyan Bista <bigyanyo@gmail.com>
2021-04-10 08:30:44 +0545

    new things

--- login.php
+++ login.php
@@ -4,19 +4,9 @@
     if (isset($_SESSION['userId'])){
         header("Location: index.php");
     }
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Form</title>
-    <script src="https://kit.fontawesome.com/10022afe1e.js" crossorigin="anonymous"></script>
-    <!-- <link rel="stylesheet" href="css/login-style.css"> -->
-</head>
-<body>
+    echo'<link rel="stylesheet" href="css/login-style.css"';
+?>
 
 <div class="test">
 <main>
--- signup.php
+++ signup.php
@@ -1,18 +1,8 @@
 <?php
     require "header.php";
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Form</title>
-    <script src="https://kit.fontawesome.com/10022afe1e.js" crossorigin="anonymous"></script>
-    <!-- <link rel="stylesheet" href="css/signup-style.css"> -->
-</head>
-<body>
+    echo'<link rel="stylesheet" href="css/signup-style.css"';
+?>
     <main>
     <div class="container">
         <div class="forms-container">
